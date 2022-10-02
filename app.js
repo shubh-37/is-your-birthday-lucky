84981383b92631b01a7d6a1eadef33d8fd58ca5c
shubh-37 <shubh.arya37@gmail.com>
2022-10-03 01:42:19 +0530

    function to add the digits in DOB

--- app.js
+++ app.js
@@ -2,8 +2,18 @@ const inputDob = document.querySelector("#date-of-birth");
 const inputLuckyNum = document.querySelector("#lucky-number");
 const checkBtn = document.querySelector("#check-dob");
 
-function clickHandler(){
-    console.log("clicked");
+function checkLuckyDob(){
+    var dob = inputDob.value;
+    var luckyNum = inputLuckyNum.value;
+    var sum = calculateSum(dob);
+}
+function calculateSum(dob){
+    var sum = 0;
+    dob = dob.replaceAll("-","");    
+    for(let i = 0; i < dob.length; i++){
+        sum = sum + Number(dob.charAt(i));
+    }
+    return sum;
 }
 
-checkBtn.addEventListener("click", clickHandler);
+checkBtn.addEventListener("click", checkLuckyDob);
