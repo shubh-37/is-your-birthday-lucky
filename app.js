1c90df5c496afb285f661f59d67ab6816ca1bdf0
shubh-37 <shubh.arya37@gmail.com>
2022-10-03 02:23:02 +0530

    function to check the dob

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 const inputDob = document.querySelector("#date-of-birth");
 const inputLuckyNum = document.querySelector("#lucky-number");
 const checkBtn = document.querySelector("#check-dob");
+const output = document.querySelector(".output-message");
+
+function compareVal(sum, luckyNum){
+    if(sum&&luckyNum){
+        if(sum%luckyNum == 0){
+            output.innerHTML = "Congrats your birthday is LUCKY🥳🥰";
+            
+        }else{
+            output.innerHTML = "Oops! sorry your birthday isnt lucky🤒😩";
+        }
+    }else{
+        output.innerHTML = "Please enter both the input to check if your birthday is lucky or not!"
+    }
+    
+}
 
 function checkLuckyDob(){
     var dob = inputDob.value;
     var luckyNum = inputLuckyNum.value;
     var sum = calculateSum(dob);
+    var output = compareVal(sum, luckyNum);
 }
 function calculateSum(dob){
     var sum = 0;
